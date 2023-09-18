e89a723bc0ca43a038973c05078a8ebeca73887d
rbdus0515 <dnqkd707@gmail.com>
2023-09-18 22:17:14 +0900

    homeupdate5

--- 포폴연습2.js
+++ 포폴연습2.js
@@ -1,30 +1,6 @@
-const darkmode = document.getElementById("dark-mode");
+const whiteMode = document.getElementById("white-mode");
 const body = document.body;
 
-darkmode.addEventListener("click", () => {
-    body.classList.toggle("dark-mode");
-});
-
-// ----------------------------------------------
-const bars = document.querySelectorAll(".skill-percent-bar .percent");
-
-bars.forEach((bar) => {
-    const skillsTotal = parseFloat(bar.getAttribute("data-percent"));
-
-    let t = 0;
-    let animationInterval;
-
-    bar.addEventListener("mouseover", () => {
-        if(!animationInterval) {
-            animationInterval = setInterval(() => {
-                if(t > skillsTotal) {
-                    clearInterval(animationInterval);
-                    animationInterval = null;
-                    t = 0;
-                } else {
-                    bar.style.width = (t++) + "%";
-                }
-            }, 10);
-        }
-    });
+whiteMode.addEventListener("click", () => {
+    body.classList.toggle("white-mode");
 });
